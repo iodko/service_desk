35f3555d00bddb924a1076efb7b01d2446f2a7b5
mamontovdn <mamontovdn@yandex.ru>
2021-06-16 17:09:54 +0300

    started registration module

---
+++ accounts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path, reverse_lazy, include
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path(
+        # Прописываю отдельно этот адресс, чтобы шаблон письма брался из html
+        # шаблона, а не .txt
+        "password/reset/",
+        auth_views.PasswordResetView.as_view(
+            success_url=reverse_lazy("auth_password_reset_done"),
+            html_email_template_name="registration/password_reset_email.html",
+        ),
+        name="auth_password_reset"
+    ),
+    path("", include("registration.backends.default.urls"))
+]
